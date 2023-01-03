972d5d34a681dd0edd6124d72fde8377411872f9
Mironov, Artem <amironov@scnsoft.com>
2023-01-03 18:40:44 +0200

    refactor(navigation): Created stack navigator using appropriated types

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,8 +1,10 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { RootStackParamList } from '@shared/lib/navigation';
+
 import LoginScreen from './LoginScreen';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default () => {
   return (
@@ -11,7 +13,7 @@ export default () => {
         options={{
           headerShown: false,
         }}
-        name="LoginScreen"
+        name="Login"
         component={LoginScreen}
       />
     </Stack.Navigator>
