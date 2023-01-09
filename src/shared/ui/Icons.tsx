3930dc6c689e056b06be8f11a3526c96fd0e1657
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-01-09 17:50:29 +0400

    refactor,simplification

--- src/shared/ui/Icons.tsx
+++ src/shared/ui/Icons.tsx
@@ -7,8 +7,6 @@ type IconProps = {
   size: number;
 };
 
-const CloseIcon: FC<IconProps> = ({ color, size }) => (
-  <AntDesign name="close" color={color} size={size} />
-);
+const CloseIcon: FC<IconProps> = props => <AntDesign name="close" {...props} />;
 
 export { CloseIcon };
