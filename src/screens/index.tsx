7093bffeb590bb3e39a294da3f11ce8cdcce0ef5
Nalivaiko, Aleksej <Nalivaiko@scnsoft.com>
2023-01-11 16:56:20 +0300

    PR based fixes

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -4,12 +4,11 @@ import {
 } from '@react-navigation/native-stack';
 import { useTranslation } from 'react-i18next';
 
-import { colors } from '@app/shared/lib';
+import { AppVersion, colors } from '@app/shared/lib';
 import { RootStackParamList } from '@shared/lib/navigation';
 import { Text, CloseIcon } from '@shared/ui';
 
-import packageJson from '../../package.json';
-import AboutScreen from './About';
+import AboutScreen from './AboutScreen';
 import ForgotPasswordScreen from './ForgotPasswordScreen';
 import LoginScreen from './LoginScreen';
 import SignUpScreen from './SignUpScreen';
@@ -72,7 +71,7 @@ export default () => {
         name="AboutApp"
         options={{
           title: t('about_app:title_with_version', {
-            version: packageJson.version,
+            version: AppVersion,
           }),
           headerTitleStyle: {
             fontSize: 16,
