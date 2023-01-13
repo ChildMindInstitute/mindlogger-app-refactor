7ef8d55970e66060cc15198583c098527c45ed63
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-01-13 14:47:02 +0400

    Added Foundation font for react-native-vector-icons, refactor

--- src/shared/ui/Icons.tsx
+++ src/shared/ui/Icons.tsx
@@ -1,16 +1,26 @@
 import { FC } from 'react';
 
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Foundation from 'react-native-vector-icons/Foundation';
 
 type IconProps = {
   color: string;
   size: number;
 };
 
-const CloseIcon: FC<IconProps> = props => <AntDesign name="close" {...props} />;
+export const CloseIcon: FC<IconProps> = props => (
+  <AntDesign name="close" {...props} />
+);
 
-const ArrowRightIcon: FC<IconProps> = props => (
+export const ArrowRightIcon: FC<IconProps> = props => (
   <AntDesign name="arrowright" {...props} />
 );
 
-export { CloseIcon, ArrowRightIcon };
+export const KeyIcon: FC<IconProps> = props => (
+  <Foundation name="key" {...props} />
+);
+
+export const UserIcon: FC<IconProps> = props => (
+  <FontAwesome name="user-circle" {...props} />
+);
