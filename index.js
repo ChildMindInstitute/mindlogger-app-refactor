b9e8cfa494b78f8bd593a5e4a78b0762d1f990fa
Mironov, Artem <amironov@scnsoft.com>
2023-01-24 14:21:48 +0200

    feat(jobs): Separated jobs on those that are foreground & background only

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
  */
 
 import { AppRegistry } from 'react-native';
+
+import { jobRunner } from './src/shared/lib';
+import requestInterception from './src/jobs/request-interception';
+import responseInterception from './src/jobs/response-interception';
+
 import App from './src/app';
 import { name as appName } from './app.json';
+
+jobRunner.runAll([requestInterception, responseInterception]);
 
 AppRegistry.registerComponent(appName, () => App);
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,11 @@
 import Screens from '@screens';
 
 import localization from '@jobs/localization';
-import requestInterception from '@jobs/request-interception';
-import responseInterception from '@jobs/response-interception';
 import { jobRunner } from '@shared/lib';
 
 import { AppProvider } from './ui';
 
-jobRunner.runAll([localization, requestInterception, responseInterception]);
+jobRunner.runAll([localization]);
 
 const App = () => {
   return (
