6943e54ff46b8634d2b6bb7bb8a701c3cf2e56e4
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-01-31 17:48:30 +0400

    M2-484/added calendar component for Applet Details screen

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -1,6 +1,10 @@
+import { format as formatBase } from 'date-fns';
+import { enGB, fr } from 'date-fns/locale';
 import i18n from 'i18next';
 
-import { Language } from './types';
+import { type Language } from './types';
+
+const dateFnsLocales = { fr, en: enGB };
 
 export const noop = () => {};
 
@@ -14,4 +18,10 @@ export function wait(milliseconds: number) {
 
 export const shuffle = <T>(array: Array<T>) => {
   return array.sort(() => 0.5 - Math.random());
+};
+
+export const format = (date: Date | number, formatStr: string) => {
+  return formatBase(date, formatStr, {
+    locale: dateFnsLocales[i18n.language as Language],
+  });
 };
