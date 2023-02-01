2e2658fbc61bfd4979bfcdd64a6e8dcdb1cf2b27
Mironov, Artem <amironov@scnsoft.com>
2023-02-01 12:53:27 +0200

    fix(session): Added error safeguards to setters

--- src/entities/session/model/operations.ts
+++ src/entities/session/model/operations.ts
@@ -1,17 +1,32 @@
-import { Emitter } from '@app/shared/lib';
+import { Emitter, throwError } from '@app/shared/lib';
 
 import storage from './storage';
 import { Session } from './types';
+import { SessionScheme } from '../validation';
 
 export function storeSession(session: Session) {
-  storage.setSession(session);
+  try {
+    SessionScheme.parse(session);
+    storage.setSession(session);
+  } catch (e: any) {
+    throwError('[storeSession]: Invalid session object has been provided');
+    throwError(e);
+  }
 }
 
 export function storeAccessToken(accessToken: string) {
+  if (!accessToken) {
+    throwError('[storeAccessToken]: No access token has been provided');
+  }
+
   storage.setSession({ accessToken });
 }
 
 export function storeRefreshToken(refreshToken: string) {
+  if (!refreshToken) {
+    throwError('[storeRefreshToken]: No refresh token has been provided');
+  }
+
   storage.setSession({ refreshToken });
 }
 
