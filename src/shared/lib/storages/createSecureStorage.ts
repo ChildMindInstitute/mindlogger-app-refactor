bc6e234da2552f7ba8a1769b94364b995c2d0d5f
Mironov, Artem <amironov@scnsoft.com>
2023-02-01 12:18:21 +0200

    fix(errors): Produce an error if encryption key has not been provided

--- src/shared/lib/storages/createSecureStorage.ts
+++ src/shared/lib/storages/createSecureStorage.ts
@@ -1,8 +1,15 @@
 import { MMKV } from 'react-native-mmkv';
 
 import { STORE_ENCRYPTION_KEY } from '../constants';
+import { throwError } from '../services';
 
 function createSecureStorage(id: string) {
+  if (!STORE_ENCRYPTION_KEY) {
+    throwError(
+      '[createSecureStorage]: STORE_ENCRYPTION_KEY has not been provided',
+    );
+  }
+
   return new MMKV({ id, encryptionKey: STORE_ENCRYPTION_KEY });
 }
 
