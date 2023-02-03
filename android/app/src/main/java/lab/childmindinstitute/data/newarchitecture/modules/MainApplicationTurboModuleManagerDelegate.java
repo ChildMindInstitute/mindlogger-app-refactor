6be7e4aed3d084b876b582e11354973ce3d733a1
Mironov, Artem <amironov@scnsoft.com>
2023-02-03 19:01:32 +0200

    build(android): renamed application id

--- android/app/src/main/java/lab/childmindinstitute/data/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/lab/childmindinstitute/data/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.mindloggermobile.newarchitecture.modules;
+package lab.childmindinstitute.data.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
