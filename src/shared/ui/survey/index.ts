c6c40026aba48445be17467e30fd39af10519684
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-03-06 14:42:38 +0400

    fixed export issue

--- src/shared/ui/survey/index.ts
+++ src/shared/ui/survey/index.ts
@@ -2,6 +2,4 @@ export { default as SimpleTextInput } from './SimpleTextInput';
 export { RadioActivityItem } from './RadioActivityItem';
 export { StackedRadiosItem } from './StackedRadiosItem';
 export { StackedCheckBoxItem } from './StackedCheckboxItem';
-export { default as SurveySlider } from './SurveySlider';
-export { default as StackedSlider } from './StackedSlider';
 export * from './Slider';
