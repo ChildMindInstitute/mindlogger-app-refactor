6309bf838c8f47edf9ae3711c5e058cc386fa2f4
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-03-10 14:27:52 +0400

    Added react-native-- webview, autoheight-webview,video,video-player packages, added video, youtube video, html_block markdown support, added VideoPlayer component, added MaterialIcons Font

--- src/shared/ui/survey/index.ts
+++ src/shared/ui/survey/index.ts
@@ -3,5 +3,6 @@ export { RadioActivityItem } from './RadioActivityItem';
 export { StackedRadiosItem } from './StackedRadiosItem';
 export { StackedCheckBoxItem } from './StackedCheckboxItem';
 export { default as AudioPlayer } from './AudioPlayer';
+export { default as VideoPlayer } from './VideoPlayer';
 export * from './Slider';
 export * from './CheckBox';
