c03b978778d88e639bef0a3a7c6b0bb66fba277b
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-03-16 14:56:50 +0400

    Implemented activity item audioStimulus component, created useAudioPlayer custom hook, refactored AudioPlayer component

---
+++ src/shared/ui/survey/AudioStimulusItem.tsx
@@ -0,0 +1,92 @@
+import { FC } from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import { useTranslation } from 'react-i18next';
+
+import { useAudioPlayer } from '@shared/lib';
+import {
+  PlayIcon,
+  StopIcon,
+  CheckIcon,
+  SpeakerIcon,
+  XStack,
+  Text,
+} from '@shared/ui';
+
+type Props = {
+  config: {
+    stimulus: string;
+    allowReplay: boolean;
+  };
+  value: boolean;
+  onChange: (value: boolean) => void;
+};
+
+const AudioStimulusItem: FC<Props> = ({ config, onChange: onFinish }) => {
+  const { t } = useTranslation();
+  const { stimulus: uri, allowReplay: replayIsAllowed } = config;
+  const { isPlaying, playbackCount, play, pause } = useAudioPlayer();
+
+  const renderIcon = () => {
+    if (isPlaying) {
+      if (replayIsAllowed) {
+        return <StopIcon size={17} color="white" />;
+      } else {
+        return <SpeakerIcon size={20} color="white" />;
+      }
+    } else if (replayIsAllowed || playbackCount === 0) {
+      return <PlayIcon size={25} color="white" />;
+    } else {
+      return <CheckIcon size={20} color="white" />;
+    }
+  };
+
+  const getButtonText = () => {
+    if (isPlaying) {
+      if (replayIsAllowed) {
+        return t('audio_player:stop');
+      } else {
+        return t('audio_player:playing');
+      }
+    } else {
+      return t('audio_player:play');
+    }
+  };
+
+  const onPress = () => {
+    const canPause = isPlaying && replayIsAllowed;
+    const canPlay = replayIsAllowed || playbackCount === 0;
+
+    if (canPause) {
+      pause();
+    } else if (canPlay) {
+      play(uri, () => onFinish(true));
+    }
+  };
+
+  const isInActive = !replayIsAllowed && (isPlaying || playbackCount > 0);
+
+  return (
+    <>
+      <TouchableOpacity disabled={isInActive} onPress={onPress}>
+        <XStack
+          h={50}
+          w={150}
+          ai="center"
+          jc="center"
+          bg="$primary"
+          p="$3"
+          opacity={isInActive ? 0.5 : 1}
+        >
+          <Text mr="$2" color="$white" fontWeight="700" fontSize={16}>
+            {getButtonText()}
+          </Text>
+
+          {renderIcon()}
+        </XStack>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+export default AudioStimulusItem;
