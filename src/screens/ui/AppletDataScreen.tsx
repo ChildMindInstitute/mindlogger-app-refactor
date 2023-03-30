7ac73041a7f2fd1991d50d092590a89cc3bd4b39
DimitrySakalouski <dsakalouski@scnsoft.com>
2023-03-30 17:01:38 +0300

    feat(data_screen): add initial solustion for timeline chart

--- src/screens/ui/AppletDataScreen.tsx
+++ src/screens/ui/AppletDataScreen.tsx
@@ -3,14 +3,38 @@ import { FC } from 'react';
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 
 import { Box, HorizontalCalendar } from '@app/shared/ui';
+import { TimelineChart } from '@app/shared/ui/charts';
 import { AppletDetailsParamList } from '@screens/config';
 
 type Props = BottomTabScreenProps<AppletDetailsParamList, 'Data'>;
+
+const mockData = [
+  {
+    date: '2023-03-27T08:36:03.931000+00:00',
+    value: 0,
+  },
+  {
+    date: '2023-03-28T08:36:03.931000+00:00',
+    value: 0,
+  },
+  {
+    date: '2023-03-30T08:36:03.931000+00:00',
+    value: 3,
+  },
+];
+
+const mockOptions = [
+  { name: 'Option 1', value: 0 },
+  { name: 'Option 2', value: 1 },
+  { name: 'Option 3', value: 2 },
+  { name: 'Option 4', value: 3 },
+];
 
 const AppletDataScreen: FC<Props> = () => {
   return (
     <Box flex={1}>
       <HorizontalCalendar mt={8} />
+      <TimelineChart data={mockData} options={mockOptions} />
     </Box>
   );
 };
