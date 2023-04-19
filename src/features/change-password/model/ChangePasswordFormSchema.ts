d285131286e63b3bffc102bd746d487e989cc51c
DimitrySakalouski <dsakalouski@scnsoft.com>
2023-04-19 09:50:15 +0300

    fix(change_password): change_password_length_validation
    https://mindlogger.atlassian.net/jira/software/c/projects/M2/issues/M2-1161

--- src/features/change-password/model/ChangePasswordFormSchema.ts
+++ src/features/change-password/model/ChangePasswordFormSchema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 const schema = z.object({
-  prev_password: z.string().min(3),
-  password: z.string().min(3),
+  prev_password: z.string().min(6),
+  password: z.string().min(6),
 });
 
 export default schema;
