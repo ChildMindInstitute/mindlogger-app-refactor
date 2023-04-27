873e6cb39925b13611c80aca8752c6a4c09662da
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-04-27 13:02:02 +0400

    Stacked checkbox pipeline item WIP

---
+++ src/features/pass-survey/model/hooks/useActivityRecordInitialization.ts
@@ -0,0 +1,67 @@
+import { useCallback, useEffect, useMemo } from 'react';
+
+import { useActivityDetailsQuery, ActivityModel } from '@app/entities/activity';
+import { useAppletDetailsQuery } from '@app/entities/applet';
+
+import { useActivityStorageRecord } from '../../lib';
+import { buildPipeline } from '../pipelineBuilder';
+
+type UseActivityRecordCreatorArgs = {
+  appletId: string;
+  activityId: string;
+  eventId: string;
+};
+
+function useActivityRecordCreator({
+  appletId,
+  activityId,
+  eventId,
+}: UseActivityRecordCreatorArgs) {
+  const { activityStorageRecord, upsertActivityStorageRecord } =
+    useActivityStorageRecord({
+      appletId,
+      activityId,
+      eventId,
+    });
+
+  const { data: appletVersion = '' } = useAppletDetailsQuery(appletId, {
+    enabled: !activityStorageRecord,
+    select: r => r.data.result.version,
+  });
+
+  let { data: activity } = useActivityDetailsQuery(activityId, {
+    enabled: !activityStorageRecord,
+    select: response => {
+      return ActivityModel.mapToActivity(response.data.result);
+    },
+  });
+
+  // @todo remove once integration is done
+  // activity = TextActivity;
+
+  const pipeline = useMemo(
+    () => (activity ? buildPipeline(activity) : []),
+    [activity],
+  );
+
+  const canCreateStorageRecord =
+    !activityStorageRecord && pipeline.length && appletVersion;
+
+  const createActivityRecord = useCallback(() => {
+    return upsertActivityStorageRecord({
+      step: 0,
+      items: pipeline,
+      answers: {},
+      appletVersion,
+      timers: {},
+    });
+  }, [appletVersion, pipeline, upsertActivityStorageRecord]);
+
+  useEffect(() => {
+    if (canCreateStorageRecord) {
+      createActivityRecord();
+    }
+  }, [canCreateStorageRecord, createActivityRecord]);
+}
+
+export default useActivityRecordCreator;
