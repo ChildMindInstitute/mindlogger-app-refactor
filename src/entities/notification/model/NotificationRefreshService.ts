2b8e8e1b4824507a11b226139bffc04eb628250f
Mironov, Artem <amironov@scnsoft.com>
2023-04-28 15:35:03 +0300

    fix(notification): Wrapped the whole refresh function into try/cactch

--- src/entities/notification/model/NotificationRefreshService.ts
+++ src/entities/notification/model/NotificationRefreshService.ts
@@ -54,83 +54,84 @@ const createNotificationRefreshService = (): NotificationRefreshService => {
       return;
     }
 
-    NotificationManager.mutex.setBusy();
+    try {
+      NotificationManager.mutex.setBusy();
 
-    const appletsResponse = getDataFromQuery<AppletsResponse>(
-      getAppletsKey(),
-      queryClient,
-    )!;
-
-    const progress = convertProgress(storeProgress);
-
-    const appletDtos: AppletDto[] = appletsResponse.result;
-
-    const applets = appletDtos.map(x => ({
-      id: x.id,
-      name: x.displayName,
-    }));
-
-    const allNotificationDescribers: NotificationDescriber[] = [];
-
-    for (let applet of applets) {
-      const detailsResponse = getDataFromQuery<AppletDetailsResponse>(
-        getAppletDetailsKey(applet.id),
+      const appletsResponse = getDataFromQuery<AppletsResponse>(
+        getAppletsKey(),
         queryClient,
       )!;
 
-      const eventsResponse = getDataFromQuery<AppletEventsResponse>(
-        getEventsKey(applet.id),
-        queryClient,
-      )!;
+      const progress = convertProgress(storeProgress);
 
-      const events: ScheduleEvent[] = mapEventsFromDto(
-        eventsResponse.result.events,
-      );
+      const appletDtos: AppletDto[] = appletsResponse.result;
 
-      const activities: Activity[] = mapActivitiesFromDto(
-        detailsResponse.result.activities,
-      );
-
-      const activityFlows: ActivityFlow[] = mapActivityFlowsFromDto(
-        detailsResponse.result.activityFlows,
-      );
-
-      const entities: Entity[] = [...activities, ...activityFlows];
-
-      const idToEntity = buildIdToEntityMap(entities);
-
-      let entityEvents = events.map<EventEntity>(event => ({
-        entity: idToEntity[event.entityId],
-        event,
+      const applets = appletDtos.map(x => ({
+        id: x.id,
+        name: x.displayName,
       }));
 
-      const calculator = EventModel.ScheduledDateCalculator;
+      const allNotificationDescribers: NotificationDescriber[] = [];
 
-      for (let eventEntity of entityEvents) {
-        const date = calculator.calculate(eventEntity.event);
-        eventEntity.event.scheduledAt = date;
+      for (let applet of applets) {
+        const detailsResponse = getDataFromQuery<AppletDetailsResponse>(
+          getAppletDetailsKey(applet.id),
+          queryClient,
+        )!;
+
+        const eventsResponse = getDataFromQuery<AppletEventsResponse>(
+          getEventsKey(applet.id),
+          queryClient,
+        )!;
+
+        const events: ScheduleEvent[] = mapEventsFromDto(
+          eventsResponse.result.events,
+        );
+
+        const activities: Activity[] = mapActivitiesFromDto(
+          detailsResponse.result.activities,
+        );
+
+        const activityFlows: ActivityFlow[] = mapActivityFlowsFromDto(
+          detailsResponse.result.activityFlows,
+        );
+
+        const entities: Entity[] = [...activities, ...activityFlows];
+
+        const idToEntity = buildIdToEntityMap(entities);
+
+        let entityEvents = events.map<EventEntity>(event => ({
+          entity: idToEntity[event.entityId],
+          event,
+        }));
+
+        const calculator = EventModel.ScheduledDateCalculator;
+
+        for (let eventEntity of entityEvents) {
+          const date = calculator.calculate(eventEntity.event);
+          eventEntity.event.scheduledAt = date;
+        }
+
+        const builder = createNotificationBuilder({
+          appletId: applet.id,
+          appletName: applet.name,
+          eventEntities: entityEvents,
+          progress,
+        });
+
+        const appletNotifications: AppletNotificationDescribers =
+          builder.build();
+
+        const filteredNotificationsArray: NotificationDescriber[] =
+          filterNotifications(appletNotifications);
+
+        allNotificationDescribers.push(...filteredNotificationsArray);
       }
 
-      const builder = createNotificationBuilder({
-        appletId: applet.id,
-        appletName: applet.name,
-        eventEntities: entityEvents,
-        progress,
-      });
+      const sortedNotificationDescribers = sortNotificationDescribers(
+        allNotificationDescribers,
+      );
 
-      const appletNotifications: AppletNotificationDescribers = builder.build();
-
-      const filteredNotificationsArray: NotificationDescriber[] =
-        filterNotifications(appletNotifications);
-
-      allNotificationDescribers.push(...filteredNotificationsArray);
-    }
-
-    const sortedNotificationDescribers = sortNotificationDescribers(
-      allNotificationDescribers,
-    );
-
-    try {
       await NotificationManager.scheduleNotifications(
         sortedNotificationDescribers,
       );
