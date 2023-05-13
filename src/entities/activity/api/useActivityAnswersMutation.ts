bdd7806f8bb9dcbd493163ec5109b933cc761db5
DimitrySakalouski <dsakalouski@scnsoft.com>
2023-05-13 17:16:22 +0300

    feat(survey_items): add empty answer case for media items

--- src/entities/activity/api/useActivityAnswersMutation.ts
+++ src/entities/activity/api/useActivityAnswersMutation.ts
@@ -11,13 +11,13 @@ type Arguments = Parameters<typeof AnswerService.sendActivityAnswers>[0];
 
 const isFileUrl = (value: string) => {
   const localFileRegex =
-    /^(file:\/\/|\/).*\/[^\/]+?\.(jpg|jpeg|png|gif|mp4|m4a|mov)$/;
+    /^(file:\/\/|\/).*\/[^\/]+?\.(jpg|jpeg|png|gif|mp4|m4a|mov|MOV)$/;
 
   return localFileRegex.test(value);
 };
 
-const sendAnswers = async (sendActivityAnswersBody: Arguments) => {
-  for (const itemAnswer of sendActivityAnswersBody.answers) {
+const sendAnswers = async (body: Arguments) => {
+  for (const itemAnswer of body.answers) {
     const { value: answerValue } = itemAnswer.answer;
 
     if (answerValue?.uri && isFileUrl(answerValue.uri)) {
@@ -27,11 +27,17 @@ const sendAnswers = async (sendActivityAnswersBody: Arguments) => {
         const { url } = uploadResult.data.result;
 
         itemAnswer.answer.value = url;
+      } else {
+        const answers = body.answers.filter(
+          answer => answer.activityItemId !== itemAnswer.activityItemId,
+        );
+
+        body.answers = answers;
       }
     }
   }
 
-  return AnswerService.sendActivityAnswers(sendActivityAnswersBody);
+  return AnswerService.sendActivityAnswers(body);
 };
 
 export const useActivityAnswersMutation = (options?: Options) => {
