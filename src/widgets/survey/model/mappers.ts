37d8b8c05c533d429d54c2dbe40f918ad390ee00
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-05-26 15:40:50 +0400

    Refactor, changed answers contract

--- src/widgets/survey/model/mappers.ts
+++ src/widgets/survey/model/mappers.ts
@@ -1,46 +1,69 @@
-import { Answers, PipelineItem } from '@app/features/pass-survey';
+import {
+  Answers,
+  PipelineItem,
+  PipelineItemAnswer,
+} from '@app/features/pass-survey';
 import { AnswerDto } from '@app/shared/api';
 
-export default function isObject<TObj>(obj: TObj) {
-  var type = typeof obj;
-  return type === 'function' || (type === 'object' && !!obj);
-}
+type Answer = PipelineItemAnswer['value'];
 
 export function mapAnswersToDto(
   pipeline: PipelineItem[],
   answers: Answers,
-  flowId: string | null,
 ): Array<AnswerDto> {
   const filteredAnswers = Object.entries(answers).filter(([_, answer]) => {
     return answer.answer != null;
   });
-  const activityItemIds = filteredAnswers.map(([step]) => {
-    return pipeline[Number(step)]?.id!;
-  });
 
   const result = filteredAnswers.map(([step, answer]) => {
-    const dto: AnswerDto = {
-      activityId: pipeline[Number(step)]?.id!,
-      flowId: flowId,
-      itemIds: activityItemIds,
-      answer: {
-        value: answer.answer as any, //TODO: fix when all types of answers DTOs are done
-      },
-    };
-
     const pipelineItem = pipeline[+step];
+    switch (pipelineItem.type) {
+      case 'TextInput':
+        return convertToTextAnswer(answer);
 
-    if (answer.additionalAnswer) {
-      dto.answer.additionalText = answer.additionalAnswer;
+      case 'Radio':
+        return convertToSingleSelectAnswer(answer);
+
+      case 'Checkbox':
+        return convertToCheckboxAnswer(answer);
+
+      case 'Slider':
+        return convertToSliderAnswer(answer);
+
+      case 'NumberSelect':
+        return null;
+
+      default:
+        return null;
     }
-
-    if (pipelineItem.type === 'TextInput') {
-      dto.answer.shouldIdentifyResponse =
-        pipelineItem.payload.shouldIdentifyResponse;
-    }
-
-    return dto;
   });
 
   return result;
+}
+
+function convertToTextAnswer(answer: Answer) {
+  return {
+    value: answer.answer,
+  };
+}
+
+function convertToSingleSelectAnswer(answer: Answer) {
+  return {
+    value: answer.answer,
+    text: answer.additionalAnswer ?? null,
+  };
+}
+
+function convertToSliderAnswer(answer: Answer) {
+  return {
+    value: answer.answer,
+    text: answer.additionalAnswer ?? null,
+  };
+}
+
+function convertToCheckboxAnswer(answer: Answer) {
+  return {
+    value: answer.answer,
+    text: answer.additionalAnswer ?? null,
+  };
 }
