c83246275d0b86ab60bfdd8a90b9b25e90b5284c
DimitrySakalouski <dsakalouski@scnsoft.com>
2023-09-12 13:38:57 +0300

    refactor(live_connection): added correct styles to connection form

--- src/features/conect-to-server/ui/ConnectionForm.tsx
+++ src/features/conect-to-server/ui/ConnectionForm.tsx
@@ -1,9 +1,10 @@
 import { FC, useState } from 'react';
+import { StyleSheet } from 'react-native';
 
 import { FormProvider } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
-import { useAppDispatch, useAppSelector } from '@app/shared/lib';
+import { colors, useAppDispatch, useAppSelector } from '@app/shared/lib';
 import { useAppForm, useTCPSocket } from '@app/shared/lib';
 import { Box, BoxProps, Text, XStack, Button } from '@app/shared/ui';
 import { CheckBoxField, InputField } from '@app/shared/ui/form';
@@ -14,6 +15,21 @@ import { ConnectionFormSchema } from '../model';
 type Props = {
   onSubmitSuccess: () => void;
 } & BoxProps;
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderBottomWidth: 1,
+    borderColor: 'grey',
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    fontSize: 16,
+  },
+  error: {
+    color: colors.errorRed,
+    fontSize: 18,
+  },
+});
 
 export const ConnectionForm: FC<Props> = ({ onSubmitSuccess, ...props }) => {
   const { t } = useTranslation();
@@ -33,6 +49,7 @@ export const ConnectionForm: FC<Props> = ({ onSubmitSuccess, ...props }) => {
       setError('Connection failed. Please double check ip and port');
     },
     onConnected: () => {
+      setError('');
       onSubmitSuccess();
     },
   });
@@ -43,17 +60,13 @@ export const ConnectionForm: FC<Props> = ({ onSubmitSuccess, ...props }) => {
 
       connect(ipAddress, +port);
 
-      if (remember) {
-        dispatch(
-          LiveConnectionModel.actions.setHistory({
-            ipAddress,
-            port,
-            remember: Boolean(remember),
-          }),
-        );
-      } else {
-        dispatch(LiveConnectionModel.actions.clearHistory());
-      }
+      dispatch(
+        LiveConnectionModel.actions.setHistory({
+          ipAddress,
+          port,
+          remember: Boolean(remember),
+        }),
+      );
     },
     defaultValues: {
       ipAddress: connection?.ipAddress,
@@ -65,10 +78,13 @@ export const ConnectionForm: FC<Props> = ({ onSubmitSuccess, ...props }) => {
     },
   });
 
-  console.log(form.formState);
-
   const disconnect = () => {
+    const remember = form.getValues('remember');
+    if (!remember) {
+      dispatch(LiveConnectionModel.actions.setDefaultHistory());
+    }
     closeConnection();
+    onSubmitSuccess();
     // TODO: add toast message
   };
 
@@ -77,9 +93,9 @@ export const ConnectionForm: FC<Props> = ({ onSubmitSuccess, ...props }) => {
       <FormProvider {...form}>
         <Text
           textAlign="center"
-          mb="$3"
+          mb={20}
           color="$darkerGrey3"
-          fontSize={24}
+          fontSize={20}
           fontWeight="900"
         >
           {t('live_connection:connect_to_server')}
@@ -89,11 +105,20 @@ export const ConnectionForm: FC<Props> = ({ onSubmitSuccess, ...props }) => {
           {t('live_connection:ip')}:
         </Text>
 
-        <Box borderColor="$darkerGrey3" borderWidth={0.5} py="$2" px="$1">
+        <Box borderColor="$darkerGrey2" px={4} borderWidth={0.5}>
           <InputField
             editable={!connected}
             name="ipAddress"
             mode="dark"
+            style={[
+              styles.input,
+              {
+                color: connected ? colors.grey2 : colors.darkerGrey2,
+                borderBottomColor: connected
+                  ? colors.grey2
+                  : colors.darkerGrey2,
+              },
+            ]}
             placeholder=""
           />
         </Box>
@@ -102,36 +127,53 @@ export const ConnectionForm: FC<Props> = ({ onSubmitSuccess, ...props }) => {
           {t('live_connection:port')}:
         </Text>
 
-        <Box
-          borderColor="$darkerGrey3"
-          borderWidth={0.5}
-          py="$2"
-          px="$1"
-          mb="$5"
-        >
+        <Box borderColor="$darkerGrey2" px={4} mb={10} borderWidth={0.5}>
           <InputField
             mode="dark"
             editable={!connected}
             name="port"
-            // keyboardType="number-pad"
+            style={[
+              styles.input,
+              {
+                color: connected ? colors.grey2 : colors.darkerGrey2,
+                borderBottomColor: connected
+                  ? colors.grey2
+                  : colors.darkerGrey2,
+              },
+            ]}
+            keyboardType="number-pad"
             placeholder=""
           />
         </Box>
 
-        <XStack ai="center" mb="$5">
-          <CheckBoxField name="remember" />
-
-          <Text fontWeight="900" color="$darkerGrey2" fontSize={16}>
-            {t('live_connection:remember')}
-          </Text>
+        <XStack ai="center">
+          <CheckBoxField
+            name="remember"
+            onCheckColor={colors.white}
+            onFillColor={colors.grey}
+            onTintColor={colors.grey}
+            tintColor={colors.grey}
+            disabled={connected}
+          >
+            <Text
+              fontWeight="900"
+              ml={12}
+              color={connected ? colors.grey2 : colors.darkerGrey2}
+              fontSize={16}
+            >
+              {t('live_connection:remember')}
+            </Text>
+          </CheckBoxField>
         </XStack>
 
+        {(error && <Text style={styles.error}>{error}</Text>) || <></>}
+
         {connected ? (
-          <Button br={4} onPress={disconnect}>
+          <Button br={4} mt={10} onPress={disconnect}>
             {t('live_connection:disconnect')}
           </Button>
         ) : (
-          <Button br={4} onPress={submit}>
+          <Button br={4} mt={10} onPress={submit}>
             {t('live_connection:connect')}
           </Button>
         )}
