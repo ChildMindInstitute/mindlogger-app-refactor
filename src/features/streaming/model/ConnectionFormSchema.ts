63c8fd786be6e8093cbb2b705c8dfdea4b966958
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-09-13 19:11:54 +0400

    feat(live_connection): bug fixes

--- src/features/streaming/model/ConnectionFormSchema.ts
+++ src/features/streaming/model/ConnectionFormSchema.ts
@@ -5,7 +5,7 @@ const schema = z.object({
   port: z
     .string()
     .transform(x => parseInt(x, 10))
-    .pipe(z.number().int().gte(1000).lte(99999))
+    .pipe(z.number().int().gte(10).lte(99999))
     .transform(x => x.toString()),
   remember: z.boolean().optional(),
 });
