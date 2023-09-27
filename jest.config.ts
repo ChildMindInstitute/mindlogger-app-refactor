24484596c0cff2c36352ab0e3ca6d2f2ae0db12a
Mironov, Artem <amironov@scnsoft.com>
2023-09-27 14:20:28 +0300

    fix(tests): Added mocks for third party services

--- jest.config.ts
+++ jest.config.ts
@@ -17,22 +17,26 @@ const jestConfig: JestConfigWithTsJest = {
   testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
   cacheDirectory: '.jest/cache',
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation|@react-native-device-info|@notifee|@miblanchard/react-native-slider|victory-*|@shopify/react-native-skia)',
+    'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation|@react-native-device-info|@notifee|@miblanchard/react-native-slider|victory-*|@shopify/react-native-skia|react-native-reanimated)',
   ],
   setupFiles: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>'],
-  moduleNameMapper: pathsToModuleNameMapper({
-    '@app/*': ['src/*'],
-    '@assets/*': ['assets/*'],
-    '@shared/*': ['src/shared/*'],
-    '@images': ['assets/images/index'],
-    '@features/*': ['src/features/*'],
-    '@screens': ['src/screens/index'],
-    '@screens/*': ['src/screens/*'],
-    '@entities/*': ['src/entities/*'],
-    '@jobs/*': ['src/jobs/*'],
-    '@widgets/*': ['src/widgets/*'],
-  }),
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper({
+      '@app/*': ['src/*'],
+      '@assets/*': ['assets/*'],
+      '@shared/*': ['src/shared/*'],
+      '@images': ['assets/images/index'],
+      '@features/*': ['src/features/*'],
+      '@screens': ['src/screens/index'],
+      '@screens/*': ['src/screens/*'],
+      '@entities/*': ['src/entities/*'],
+      '@jobs/*': ['src/jobs/*'],
+      '@widgets/*': ['src/widgets/*'],
+    }),
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|html)$':
+      '<rootDir>/assetsTransformer.js',
+  },
 };
 
 export default jestConfig;
