4a3e6f590876f6c3f1fd8ae2f1f2302e6e538658
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-09-29 03:18:14 +0400

    Fixed M2-1979 drawing and AbTrails blinking issue

--- src/entities/abTrail/ui/AbCanvas.tsx
+++ src/entities/abTrail/ui/AbCanvas.tsx
@@ -122,12 +122,21 @@ const AbCanvas: FC<Props> = props => {
     return !!node && node.orderIndex === nodeIndexToCheck;
   };
 
+  const isCloseToNode = (pointToCheck: Point, nodeIndexToCheck: number) => {
+    const node = findNodeByPoint(pointToCheck, 2);
+    return !!node && node.orderIndex === nodeIndexToCheck;
+  };
+
   const isOverCurrent = (point: Point) => {
     return isOverNode(point, getCurrentIndex());
   };
 
   const isOverNext = (point: Point) => {
     return isOverNode(point, getCurrentIndex() + 1);
+  };
+
+  const isCloseToNext = (point: Point) => {
+    return isCloseToNode(point, getCurrentIndex() + 1);
   };
 
   const isOverLast = (point: Point) => {
@@ -162,13 +171,16 @@ const AbCanvas: FC<Props> = props => {
   const findNodeByIndex = (index: number) =>
     canvasData!.nodes.find(x => x.orderIndex === index)!;
 
-  const findNodeByPoint = (point: Point): TestNode | null => {
+  const findNodeByPoint = (
+    point: Point,
+    radiusMultiplier = 1,
+  ): TestNode | null => {
     const foundNode = canvasData?.nodes.find(node => {
       const distance = getDistance(
         { x: node.cx, y: node.cy },
         { x: point.x, y: point.y },
       );
-      return distance < canvasData.config.radius;
+      return distance < canvasData.config.radius * radiusMultiplier;
     });
     return foundNode ?? null;
   };
@@ -272,6 +284,10 @@ const AbCanvas: FC<Props> = props => {
 
     drawPath();
 
+    if (isCloseToNext(point)) {
+      reRender();
+    }
+
     onLog({
       x: (touchInfo.x * width) / 100,
       y: (touchInfo.y * width) / 100,
@@ -358,6 +374,7 @@ const AbCanvas: FC<Props> = props => {
       {canvasData && (
         <AbShapes
           paths={paths}
+          lastPath={currentPathRef.current}
           testData={canvasData}
           greenRoundOrder={getGreenPointIndex()}
           errorPath={errorPath}
