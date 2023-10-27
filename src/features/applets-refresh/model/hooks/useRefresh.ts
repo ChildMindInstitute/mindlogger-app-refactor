d9f66eba5e1b1bf74806a8a2027d25c119cd8e7b
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-10-27 13:55:52 +0400

    Fixed M2-3914 issue with RefreshControl on ios when opening app from background

--- src/features/applets-refresh/model/hooks/useRefresh.ts
+++ src/features/applets-refresh/model/hooks/useRefresh.ts
@@ -2,10 +2,14 @@ import { useState } from 'react';
 
 import { AppletModel } from '@entities/applet';
 
+import useRefreshResumeOnAppState from './useRefreshResumeOnAppState';
+
 function useRefresh(onSuccess: () => void) {
   const { mutateAsync: refresh } = AppletModel.useRefreshMutation(onSuccess);
 
   const [isRefreshing, setIsRefreshing] = useState(false);
+
+  useRefreshResumeOnAppState(isRefreshing, setIsRefreshing);
 
   const onRefresh = () => {
     refresh().then(() => setIsRefreshing(false));
