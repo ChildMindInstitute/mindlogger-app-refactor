680ce6e329400cd02938fc409a9eacc3e35c6e25
DimitrySakalouski <dsakalouski@scnsoft.com>
2023-10-27 14:12:03 +0300

    feat: added new UI for login screen

--- src/screens/ui/LoginScreen.tsx
+++ src/screens/ui/LoginScreen.tsx
@@ -7,31 +7,27 @@ import {
 } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
-import { styled } from '@tamagui/core';
 import { useTranslation } from 'react-i18next';
 
 import { LoginForm } from '@features/login';
-import { Center, Text, Image, YStack, XStack, Box } from '@shared/ui';
+import {
+  Link,
+  Image,
+  YStack,
+  XStack,
+  Box,
+  SubmitButton,
+  Center,
+} from '@shared/ui';
 
-import { whiteLogo } from '@images';
-
-const Link = styled(Text, { color: '$secondary' });
+import { cloudLogo } from '@images';
 
 const LoginScreen: FC = () => {
   const { navigate } = useNavigation();
   const { t } = useTranslation();
-  const title = 'MindLogger';
 
   const navigateToSignUp = () => {
     navigate('SignUp');
-  };
-
-  const navigateToForgotPassword = () => {
-    navigate('ForgotPassword');
-  };
-
-  const navigateToAbout = () => {
-    navigate('AboutApp');
   };
 
   const navigateToAppLanguage = () => {
@@ -51,42 +47,51 @@ const LoginScreen: FC = () => {
       <Box flex={1} bg="$primary">
         <StatusBar />
 
-        <Box flex={1} jc="center">
-          <YStack space={30}>
-            <Link
-              color="$secondary"
-              fontWeight="400"
-              fontSize={40}
-              alignSelf="center"
-            >
-              {title}
-            </Link>
+        <Box flex={1} py="$8">
+          <YStack space={30} f={1}>
+            <YStack f={2} jc="space-between" h="100%">
+              <Center f={1} pt="$5">
+                <Image
+                  alignSelf="center"
+                  src={cloudLogo}
+                  width={310}
+                  height={55}
+                />
+              </Center>
 
-            <LoginForm px="$8" onLoginSuccess={onLoginSuccess} />
+              <LoginForm px="$8" pt="$3" onLoginSuccess={onLoginSuccess} />
+            </YStack>
 
-            <Center space>
-              <XStack space>
-                <Link onPress={navigateToSignUp}>{t('login:new_user')}</Link>
+            <YStack flex={1} px="$8" jc="space-between">
+              <SubmitButton
+                borderRadius={30}
+                width="100%"
+                backgroundColor="$primary"
+                borderColor="$white"
+                borderWidth={1}
+                buttonStyle={{ paddingVertical: 14 }}
+                textProps={{
+                  fontSize: 14,
+                  color: '$white',
+                }}
+                onPress={navigateToSignUp}
+              >
+                {t('login:account_create')}
+              </SubmitButton>
 
-                <Link onPress={navigateToForgotPassword}>
-                  {t('login:forgot_password')}
+              <XStack jc="space-between">
+                <Link
+                  textDecorationLine="underline"
+                  onPress={navigateToAppLanguage}
+                >
+                  {t('language_screen:change_app_language')}
+                </Link>
+
+                <Link textDecorationLine="underline" onPress={navigateToTerms}>
+                  {t('auth:terms_of_service')}
                 </Link>
               </XStack>
-
-              <Link onPress={navigateToAbout}>{`${t(
-                'login:what_is',
-              )} ${title}?`}</Link>
-
-              <Link onPress={navigateToAppLanguage}>
-                {t('language_screen:change_app_language')}
-              </Link>
-
-              <Link onPress={navigateToTerms}>
-                {t('auth:terms_of_service')}
-              </Link>
-            </Center>
-
-            <Image alignSelf="center" src={whiteLogo} width={70} height={70} />
+            </YStack>
           </YStack>
         </Box>
       </Box>
