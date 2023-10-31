dab42da4e0896606154d027c2e61ce37e36767a2
DimitrySakalouski <dsakalouski@scnsoft.com>
2023-10-31 15:46:21 +0300

    fix: login screen UI issues

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,6 +1,7 @@
+import { CloudLogo } from './CloudLogo';
+
 const badge = require('./badge.png');
 const blueLogo = require('./blue_logo.png');
-const cloudLogo = require('./cloud_logo.png');
 const whiteLogo = require('./white_logo.png');
 
-export { whiteLogo, blueLogo, badge, cloudLogo };
+export { whiteLogo, blueLogo, badge, CloudLogo };
