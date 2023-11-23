1f7520d53354b6caaa86feee472358ca23dede87
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2023-11-23 14:21:47 +0400

    Code improvement

--- src/shared/ui/charts/lib/getWeekDaysWithLocale.ts
+++ src/shared/ui/charts/lib/getWeekDaysWithLocale.ts
@@ -1,9 +1,15 @@
 import i18n from 'i18next';
 
-import { DAYS_OF_WEEK_SHORT_NAMES } from '@shared/lib';
-
 export const getWeekDaysWithLocale = () => {
-  const { resolvedLanguage } = i18n;
+  const DAYS_OF_WEEK_SHORT_NAMES = [
+    i18n.t('activity_chart:monday'),
+    i18n.t('activity_chart:tuesday'),
+    i18n.t('activity_chart:wednesday'),
+    i18n.t('activity_chart:thursday'),
+    i18n.t('activity_chart:friday'),
+    i18n.t('activity_chart:saturday'),
+    i18n.t('activity_chart:sunday'),
+  ];
 
-  return DAYS_OF_WEEK_SHORT_NAMES[resolvedLanguage as 'fr' | 'en'];
+  return DAYS_OF_WEEK_SHORT_NAMES;
 };
