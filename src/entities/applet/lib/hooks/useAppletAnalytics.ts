c057a0373974070cbf9894d85938bfbcae70d83f
Mironov, Artem <amironov@scnsoft.com>
2023-12-08 14:34:55 +0200

    perf(applet-analytics): Fixed long loading of a screen

--- src/entities/applet/lib/hooks/useAppletAnalytics.ts
+++ src/entities/applet/lib/hooks/useAppletAnalytics.ts
@@ -1,4 +1,5 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { InteractionManager } from 'react-native';
 
 import { IdentityModel, UserPrivateKeyRecord } from '@app/entities/identity';
 import {
@@ -6,10 +7,17 @@ import {
   formatToISODate,
   getCurrentWeekDates,
   useAppSelector,
+  wait,
 } from '@app/shared/lib';
 
 import { useAppletAnalyticsQuery, useAppletDetailsQuery } from '../../api';
 import { mapAppletAnalytics, mapAppletDetailsFromDto } from '../../model';
+import { ActivityResponses } from '../types';
+
+type AppletActivitiesResponses = {
+  id: string;
+  activitiesResponses: ActivityResponses[];
+};
 
 export const useAppletAnalytics = (appletId: string) => {
   const currentWeekDates = getCurrentWeekDates();
@@ -24,38 +32,43 @@ export const useAppletAnalytics = (appletId: string) => {
 
   const userPrivateKey = useMemo(() => UserPrivateKeyRecord.get(), []);
 
-  const { data: appletAnalytics, isLoading: isAnalyticsLoading } =
-    useAppletAnalyticsQuery(
-      {
+  const [appletAnalytics, setAppletAnalytics] =
+    useState<AppletActivitiesResponses>();
+
+  const { data: analyticsResponse, isFetching: isAnalyticsLoading } =
+    useAppletAnalyticsQuery({
+      appletId,
+      fromDate: formatToISODate(firstDateOfCurrentWeek),
+      respondentIds: respondentId ?? '',
+      isLastVersion: true,
+    });
+
+  useEffect(() => {
+    if (!analyticsResponse || !appletEncryption || !userPrivateKey) {
+      return;
+    }
+
+    InteractionManager.runAfterInteractions(async () => {
+      // Add delay as the encryption blocks the JS thread causing a long loading.
+      await wait(100);
+
+      const encryptionService = encryption.createEncryptionService({
+        prime: appletEncryption.prime,
+        publicKey: appletEncryption.publicKey,
+        base: appletEncryption.base,
+        privateKey: userPrivateKey,
+      });
+
+      const analytics = mapAppletAnalytics({
         appletId,
-        fromDate: formatToISODate(firstDateOfCurrentWeek),
-        respondentIds: respondentId ?? '',
-        isLastVersion: true,
-      },
-      {
-        select: response => {
-          if (!appletEncryption || !userPrivateKey) {
-            return;
-          }
+        activitiesDto: analyticsResponse.data.result.activities,
+        answersDto: analyticsResponse.data.result.answers,
+        encryptionService,
+      });
 
-          const encryptionService = encryption.createEncryptionService({
-            prime: appletEncryption.prime,
-            publicKey: appletEncryption.publicKey,
-            base: appletEncryption.base,
-            privateKey: userPrivateKey,
-          });
-
-          const analytics = mapAppletAnalytics({
-            appletId,
-            activitiesDto: response.data.result.activities,
-            answersDto: response.data.result.answers,
-            encryptionService,
-          });
-
-          return analytics;
-        },
-      },
-    );
+      setAppletAnalytics(analytics);
+    });
+  }, [analyticsResponse, appletEncryption, appletId, userPrivateKey]);
 
   return {
     analytics: appletAnalytics ?? null,
