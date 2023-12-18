39321172db3c6eb9e26828360e6b643ff4086c19
Mironov, Artem <amironov@scnsoft.com>
2023-12-18 15:13:10 +0200

    Added TEMPORARY hardcoded DR API server URL

--- src/shared/api/services/httpService.ts
+++ src/shared/api/services/httpService.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-import { API_URL } from '@shared/lib';
+// import { API_URL } from '@shared/lib';
 
 const httpService = axios.create({
-  baseURL: API_URL,
+  // * [TODO] Revert it later when the backend changes
+  // * regarding LORIS integration are merged to dev
+  // baseURL: API_URL,
+  baseURL: 'https://api-dr.cmiml.net',
   withCredentials: true,
 });
 
