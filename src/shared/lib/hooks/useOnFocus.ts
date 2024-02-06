157880013c543dc99f27c3ca563d403f9a2712e6
Nalivaiko, Aleksej <Nalivaiko@scnsoft.com>
2024-02-06 14:07:10 +0300

    PR fix

--- src/shared/lib/hooks/useOnFocus.ts
+++ src/shared/lib/hooks/useOnFocus.ts
@@ -1,18 +1,14 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useRef } from 'react';
 
-import { useIsFocused } from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native';
 
 const useOnFocus = (callback: () => void) => {
-  const isFocused = useIsFocused();
-
   const callbackRef = useRef(callback);
   callbackRef.current = callback;
 
-  useEffect(() => {
-    if (isFocused) {
-      callbackRef.current();
-    }
-  }, [isFocused]);
+  const focusHandler = useCallback(() => callbackRef.current(), []);
+
+  useFocusEffect(focusHandler);
 };
 
 export default useOnFocus;
