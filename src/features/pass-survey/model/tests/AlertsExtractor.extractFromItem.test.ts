ad4549c4672a673bc5f9d2ba6c99d7ab1e05a1e4
Nalivaiko, Aleksej <Nalivaiko@scnsoft.com>
2024-03-11 12:14:51 +0300

    PR fixes

--- src/features/pass-survey/model/tests/AlertsExtractor.extractFromItem.test.ts
+++ src/features/pass-survey/model/tests/AlertsExtractor.extractFromItem.test.ts
@@ -22,15 +22,6 @@ const mockExtractFunctions = (extractor: AlertsExtractor) => {
   extractor.extractFromStackedCheckbox = extractFromStackedCheckboxMock;
   extractor.extractFromStackedRadio = extractFromStackedRadioMock;
   extractor.extractFromStackedSlider = extractFromStackedSliderMock;
-
-  return {
-    extractFromRadioMock,
-    extractFromCheckboxMock,
-    extractFromStackedRadioMock,
-    extractFromStackedCheckboxMock,
-    extractFromSliderMock,
-    extractFromStackedSliderMock,
-  };
 };
 
 describe('AlertsExtractor: test extractFromItem', () => {
@@ -43,92 +34,73 @@ describe('AlertsExtractor: test extractFromItem', () => {
       error: jest.fn(),
       info: jest.fn(),
     } as unknown as ILogger);
+
+    mockExtractFunctions(extractor);
   });
 
   it('Should call extractFromRadio when type is Radio', () => {
-    const { extractFromRadioMock } = mockExtractFunctions(extractor);
-
     //@ts-expect-error
     extractor.extractFromItem({ type: 'Radio' } as PipelineItem, {} as Answer);
 
-    expect(extractFromRadioMock).toBeCalledTimes(1);
+    expect(extractor.extractFromRadio).toBeCalledTimes(1);
   });
 
   it('Should call extractFromCheckbox when type is Checkbox', () => {
-    const { extractFromCheckboxMock } = mockExtractFunctions(extractor);
-
     //@ts-expect-error
     extractor.extractFromItem(
       { type: 'Checkbox' } as PipelineItem,
       {} as Answer,
     );
 
-    expect(extractFromCheckboxMock).toBeCalledTimes(1);
+    expect(extractor.extractFromCheckbox).toBeCalledTimes(1);
   });
 
   it('Should call extractFromSlider when type is Slider', () => {
-    const { extractFromSliderMock } = mockExtractFunctions(extractor);
-
     //@ts-expect-error
     extractor.extractFromItem({ type: 'Slider' } as PipelineItem, {} as Answer);
 
-    expect(extractFromSliderMock).toBeCalledTimes(1);
+    expect(extractor.extractFromSlider).toBeCalledTimes(1);
   });
 
   it('Should call extractFromStackedRadio when type is StackedRadio', () => {
-    const { extractFromStackedRadioMock } = mockExtractFunctions(extractor);
-
     //@ts-expect-error
     extractor.extractFromItem(
       { type: 'StackedRadio' } as PipelineItem,
       {} as Answer,
     );
 
-    expect(extractFromStackedRadioMock).toBeCalledTimes(1);
+    expect(extractor.extractFromStackedRadio).toBeCalledTimes(1);
   });
 
   it('Should call extractFromStackedCheckbox when type is StackedCheckbox', () => {
-    const { extractFromStackedCheckboxMock } = mockExtractFunctions(extractor);
-
     //@ts-expect-error
     extractor.extractFromItem(
       { type: 'StackedCheckbox' } as PipelineItem,
       {} as Answer,
     );
 
-    expect(extractFromStackedCheckboxMock).toBeCalledTimes(1);
+    expect(extractor.extractFromStackedCheckbox).toBeCalledTimes(1);
   });
 
   it('Should call extractFromStackedSlider when type is StackedSlider', () => {
-    const { extractFromStackedSliderMock } = mockExtractFunctions(extractor);
-
     //@ts-expect-error
     extractor.extractFromItem(
       { type: 'StackedSlider' } as PipelineItem,
       {} as Answer,
     );
 
-    expect(extractFromStackedSliderMock).toBeCalledTimes(1);
+    expect(extractor.extractFromStackedSlider).toBeCalledTimes(1);
   });
 
   it('Should not call anything when type is Audio', () => {
-    const {
-      extractFromRadioMock,
-      extractFromCheckboxMock,
-      extractFromStackedRadioMock,
-      extractFromStackedCheckboxMock,
-      extractFromSliderMock,
-      extractFromStackedSliderMock,
-    } = mockExtractFunctions(extractor);
-
     //@ts-expect-error
     extractor.extractFromItem({ type: 'Audio' } as PipelineItem, {} as Answer);
 
-    expect(extractFromRadioMock).toBeCalledTimes(0);
-    expect(extractFromCheckboxMock).toBeCalledTimes(0);
-    expect(extractFromStackedRadioMock).toBeCalledTimes(0);
-    expect(extractFromStackedCheckboxMock).toBeCalledTimes(0);
-    expect(extractFromSliderMock).toBeCalledTimes(0);
-    expect(extractFromStackedSliderMock).toBeCalledTimes(0);
+    expect(extractor.extractFromRadio).toBeCalledTimes(0);
+    expect(extractor.extractFromCheckbox).toBeCalledTimes(0);
+    expect(extractor.extractFromStackedRadio).toBeCalledTimes(0);
+    expect(extractor.extractFromStackedCheckbox).toBeCalledTimes(0);
+    expect(extractor.extractFromSlider).toBeCalledTimes(0);
+    expect(extractor.extractFromStackedSlider).toBeCalledTimes(0);
   });
 });
