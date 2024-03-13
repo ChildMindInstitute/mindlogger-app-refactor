a7dac0241ab5d1862ad029b8b3faebd627151709
sultanofcardio <kenroy.gobourne@gmail.com>
2024-03-13 03:46:41 -0500

    M2-5065: Fallback to browser when link doesn't match

--- src/app/ui/AppProvider/NavigationProvider.tsx
+++ src/app/ui/AppProvider/NavigationProvider.tsx
@@ -1,10 +1,12 @@
 import { FC, PropsWithChildren } from 'react';
+import { Linking } from 'react-native';
 
 import {
   NavigationContainer,
   NavigationState,
   DefaultTheme,
   LinkingOptions,
+  getStateFromPath,
 } from '@react-navigation/native';
 import { ScreensModel, RootStackParamList } from '@screens';
 import Config from 'react-native-config';
@@ -21,6 +23,19 @@ const theme = {
 
 const linking: LinkingOptions<ReactNavigation.RootParamList> = {
   prefixes: [DEEP_LINK_PREFIX!],
+  getStateFromPath: (path, options) => {
+    const state = getStateFromPath(path, options);
+    if (!state) {
+      console.log(
+        `No matching route found, open URL in browser: ${DEEP_LINK_PREFIX}/${path}`,
+      );
+      // No matching route found, open URL in browser
+      Linking.openURL(`${DEEP_LINK_PREFIX}/${path}`).catch(err =>
+        console.error('An error occurred', err),
+      );
+    }
+    return state;
+  },
   config: {
     screens: {
       PasswordRecovery: 'password-recovery',
