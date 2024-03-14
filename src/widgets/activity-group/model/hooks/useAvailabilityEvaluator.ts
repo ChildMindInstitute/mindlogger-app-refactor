a77d9cad4562b980a3a72ce50806bcabcd6af338
Mironov, Artem <amironov@scnsoft.com>
2024-03-14 15:21:21 +0100

    refactor(activity-groups): Used static methods of GroupUtility to get rid of unnecessary dependencies

--- src/widgets/activity-group/model/hooks/useAvailabilityEvaluator.ts
+++ src/widgets/activity-group/model/hooks/useAvailabilityEvaluator.ts
@@ -1,32 +1,18 @@
 import { useQueryClient } from '@tanstack/react-query';
-import { useSelector } from 'react-redux';
 
-import { convertProgress } from '@app/abstract/lib';
-import { AppletModel } from '@app/entities/applet';
 import { EventModel } from '@app/entities/event';
 import { QueryDataUtils } from '@app/shared/api';
 
 import { GroupUtility } from '../factories';
-import { mapActivitiesFromDto } from '../mappers';
 
 export function useAvailabilityEvaluator() {
   const queryClient = useQueryClient();
 
-  const storeProgress = useSelector(
-    AppletModel.selectors.selectInProgressApplets,
-  );
-
   function evaluateAvailableTo(appletId: string, eventId: string): Date | null {
     const queryDataUtils = new QueryDataUtils(queryClient);
-    const appletDto = queryDataUtils.getAppletDto(appletId);
-    const activities = mapActivitiesFromDto(appletDto!.activities);
 
     const availableToEvaluator = new EventModel.AvailableToEvaluator(
-      new GroupUtility({
-        allAppletActivities: activities,
-        appletId: appletId,
-        progress: convertProgress(storeProgress),
-      }),
+      GroupUtility,
     );
 
     const eventDto = queryDataUtils.getEventDto(appletId, eventId);
