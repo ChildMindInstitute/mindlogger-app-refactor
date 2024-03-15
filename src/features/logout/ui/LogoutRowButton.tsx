836a5b2c642e941b5fa8b5f2eae5686fc5a48480
Mironov, Artem <amironov@scnsoft.com>
2024-03-15 16:39:01 +0100

    feat(logout): Added a loading view for logout
    https://mindlogger.atlassian.net/browse/M2-5879

--- src/features/logout/ui/LogoutRowButton.tsx
+++ src/features/logout/ui/LogoutRowButton.tsx
@@ -6,13 +6,22 @@ import { RowButton, KeyIcon } from '@shared/ui';
 
 import { useLogout } from '../model';
 
-const LogoutRowButton: FC = () => {
+type Props = {
+  onPress: () => void;
+};
+
+const LogoutRowButton: FC<Props> = ({ onPress }) => {
   const { t } = useTranslation();
   const { logout } = useLogout();
 
+  function performLogout() {
+    logout();
+    onPress();
+  }
+
   return (
     <RowButton
-      onPress={logout}
+      onPress={performLogout}
       accessibilityLabel="logout-button"
       title={t('settings:logout')}
       rightIcon={KeyIcon}
