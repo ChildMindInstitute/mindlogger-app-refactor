8df01636813738880af0d7c6fc5ea5558c8fb34d
sultanofcardio <kenroy.gobourne@gmail.com>
2024-03-20 10:34:44 -0500

    M2-5065: Replace console logs with Logger statements
    Also remove redundant import of Config, as the DEEP_LINK_PREFIX is already imported

--- src/app/ui/AppProvider/NavigationProvider.tsx
+++ src/app/ui/AppProvider/NavigationProvider.tsx
@@ -9,9 +9,10 @@ import {
   getStateFromPath,
 } from '@react-navigation/native';
 import { ScreensModel, RootStackParamList } from '@screens';
-import Config from 'react-native-config';
 
-import { DEEP_LINK_PREFIX } from '@app/shared/lib';
+import { DEEP_LINK_PREFIX, Logger } from '@app/shared/lib';
+
+const LOGGER_MODULE_NAME = 'NavigationProvider';
 
 const theme = {
   ...DefaultTheme,
@@ -26,12 +27,15 @@ const linking: LinkingOptions<ReactNavigation.RootParamList> = {
   getStateFromPath: (path, options) => {
     const state = getStateFromPath(path, options);
     if (!state) {
-      console.log(
-        `No matching route found, open URL in browser: ${DEEP_LINK_PREFIX}/${path}`,
+      Logger.warn(
+        `[${LOGGER_MODULE_NAME}] No matching route found, open URL in browser: ${DEEP_LINK_PREFIX}/${path}`,
       );
+
       // No matching route found, open URL in browser
       Linking.openURL(`${DEEP_LINK_PREFIX}/${path}`).catch(err =>
-        console.error('An error occurred', err),
+        Logger.error(
+          `[${LOGGER_MODULE_NAME}] An error occurred opening deep link ${DEEP_LINK_PREFIX}/${path} in the browser:\n\n ${err}`,
+        ),
       );
     }
     return state;
@@ -47,7 +51,7 @@ const NavigationProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
     <NavigationContainer
       theme={theme}
-      linking={Config.DEEP_LINK_PREFIX ? linking : undefined}
+      linking={DEEP_LINK_PREFIX ? linking : undefined}
       onStateChange={state =>
         ScreensModel.onScreenChanged(
           // @react-navigation's poor type inference
