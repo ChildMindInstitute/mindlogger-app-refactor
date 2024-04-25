9f3fc1dbcec6ffd0dd95bbfb3cbc1ebd1b751d70
vmkhitaryanscn <120190555+vmkhitaryanscn@users.noreply.github.com>
2024-04-25 12:05:41 +0400

    M2-6303: Unit tests for widgets survey mapAnswersToDto function (#738)
    * M2-6303: Unit tests for widgets survey mapAnswersToDto function

---
+++ src/widgets/survey/model/mappers.output.mock.ts
@@ -0,0 +1,107 @@
+export const textOutput = ['kkkkk'];
+export const checkboxOutput = [
+  {
+    value: [1, 2],
+  },
+];
+
+export const radioOutput = [
+  {
+    value: 2,
+  },
+];
+
+export const numberSelectOutput = [{ value: '2' }];
+
+export const sliderOutput = [
+  {
+    value: 4,
+  },
+];
+
+export const additionalAnswerOutput = [
+  {
+    text: 'Additional',
+    value: 2,
+  },
+];
+
+export const answersWithEmptyEntries = ['kkkkk', null, null, null, null];
+
+export const dateOutput = [
+  {
+    value: {
+      day: 12,
+      month: 12,
+      year: 2020,
+    },
+  },
+];
+
+export const timeOutput = [
+  {
+    value: {
+      hours: 22,
+      minutes: 22,
+    },
+  },
+];
+
+export const timeRangeOutput = [
+  {
+    value: {
+      from: {
+        hour: 22,
+        minute: 22,
+      },
+      to: {
+        hour: 23,
+        minute: 23,
+      },
+    },
+  },
+];
+
+export const geoOutput = [
+  {
+    value: {
+      latitude: 1,
+      longitude: 2,
+    },
+  },
+];
+
+export const stackedRadioOutput = [
+  {
+    value: ['One'],
+  },
+];
+
+export const stackedCheckboxOutput = [
+  {
+    value: [['opt1'], ['opt2']],
+  },
+];
+export const stackedSliderOutput = [
+  {
+    value: [1, 9],
+  },
+];
+
+export const mediaOutput = [
+  {
+    value: {
+      fileName: 'some-name.mp4',
+      fromLibrary: true,
+      size: 100,
+      type: 'mp4',
+      uri: 'some://uri',
+    },
+  },
+];
+
+export const audioPlayerOutput = [
+  {
+    value: true,
+  },
+];
