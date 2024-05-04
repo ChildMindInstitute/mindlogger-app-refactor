b4f25683c95f48de819d682401895f36a1a71394
Vardan Mkhitaryan <vmkhitaryan@scnsoft.com>
2024-05-04 13:58:01 +0400

    M2-6360:  Unit tests for entities applet mappers

---
+++ src/entities/applet/model/tests/mappers.input.mock.ts
@@ -0,0 +1,331 @@
+import {
+  ActivityDto,
+  ActivityFlowRecordDto,
+  ActivityItemDto,
+  ActivityRecordDto,
+  AnalyticsAnswerDto,
+  AppletDetailsDto,
+  AppletDetailsResponse,
+  AppletDto,
+  CompletedEntityDto,
+  ThemeDto,
+} from '@shared/api';
+
+export const appletDetailsResponse: AppletDetailsResponse = {
+  // @ts-ignore
+  result: undefined,
+  respondentMeta: {
+    nickname: 'Some nick',
+  },
+};
+
+export const completedEntityDto: CompletedEntityDto = {
+  id: '0',
+  answerId: '1',
+  submitId: '2',
+  scheduledEventId: '3',
+  localEndDate: '2020-04-04',
+  localEndTime: '20:00',
+};
+
+// @ts-expect-error
+export const themeDtoWithEmptyImages: ThemeDto = {
+  id: '1',
+  name: 'theme',
+  primaryColor: '#f00',
+  secondaryColor: '#0f0',
+  tertiaryColor: '#00f',
+};
+
+export const themeDto: ThemeDto = {
+  ...themeDtoWithEmptyImages,
+  logo: 'https://logoimage',
+  backgroundImage: 'https://backgroundImage',
+};
+
+export const firstAppletDto: AppletDto = {
+  id: '1',
+  image: 'https://appletImage',
+  displayName: 'displayName',
+  description: 'description',
+  theme: themeDto,
+  version: '1.1.11',
+  about: 'About text',
+  watermark: 'https://watermark',
+};
+
+export const secondAppletDto: AppletDto = {
+  id: '2',
+  image: 'https://appletImagesecond',
+  displayName: 'displayName second',
+  description: 'description second',
+  theme: themeDtoWithEmptyImages,
+  version: '2.2.2222',
+  about: 'About text second applet',
+  watermark: 'https://watermarksecond',
+};
+
+export const activityRecordsDto: ActivityRecordDto[] = [
+  {
+    id: '1',
+    name: 'name',
+    description: 'description',
+    image: null,
+    isReviewable: false,
+    isSkippable: false,
+    showAllAtOnce: false,
+    isHidden: false,
+    responseIsEditable: false,
+    order: 2,
+    splashScreen: 'https://splash',
+  },
+  {
+    id: '2',
+    name: 'name',
+    description: 'description',
+    image: null,
+    isReviewable: false,
+    isSkippable: false,
+    showAllAtOnce: false,
+    isHidden: false,
+    responseIsEditable: false,
+    order: 1,
+    splashScreen: 'https://splash',
+  },
+];
+
+const activityFlows: ActivityFlowRecordDto[] = [
+  {
+    id: '100',
+    name: 'name',
+    description: 'description',
+    hideBadge: false,
+    isSingleReport: false,
+    order: 0,
+    isHidden: false,
+    activityIds: ['1', '2'],
+  },
+];
+export const appletDetailsDto: AppletDetailsDto = {
+  ...firstAppletDto,
+  encryption: {
+    accountId: 'accountId',
+    base: 'Base10',
+    prime: 'Prime10',
+    publicKey: 'pub10',
+  },
+  streamEnabled: true,
+  streamIpAddress: '127.0.0.1',
+  streamPort: null,
+  activities: activityRecordsDto,
+  activityFlows: activityFlows,
+};
+
+const activityItems: ActivityItemDto[] = [
+  {
+    question: 'Checkbox',
+    responseType: 'multiSelect',
+    responseValues: {
+      options: [
+        {
+          id: 'string',
+          text: 'string',
+          image: null,
+          score: null,
+          color: null,
+          tooltip: 'string',
+          value: 1,
+          isNoneAbove: true,
+          isHidden: false,
+          alert: null,
+        },
+      ],
+    },
+    config: {
+      setPalette: false,
+      additionalResponseOption: {
+        textInputOption: false,
+        textInputRequired: false,
+      },
+      removeBackButton: false,
+      skippableItem: false,
+      timer: 10,
+      addScores: false,
+      setAlerts: false,
+      addTooltip: false,
+      randomizeOptions: false,
+    },
+    name: 'Screen2',
+    isHidden: false,
+    conditionalLogic: null,
+    id: '1',
+    timer: null,
+    order: 1,
+  },
+
+  {
+    timer: null,
+    question: 'Slider',
+    responseType: 'slider',
+    responseValues: {
+      minLabel: 'min label',
+      maxLabel: 'max label',
+      minImage: null,
+      maxImage: null,
+      minValue: 0,
+      maxValue: 10,
+      alerts: [
+        {
+          value: 1,
+          minValue: 0,
+          maxValue: 10,
+          alert: 'Some alert',
+        },
+        {
+          value: 2,
+          minValue: 5,
+          maxValue: 7,
+          alert: 'Some alert 2',
+        },
+      ],
+      scores: [0, 1],
+    },
+    config: {
+      additionalResponseOption: {
+        textInputOption: false,
+        textInputRequired: false,
+      },
+      removeBackButton: false,
+      skippableItem: false,
+      timer: null,
+      addScores: false,
+      setAlerts: false,
+      showTickMarks: true,
+      showTickLabels: false,
+      continuousSlider: true,
+    },
+    name: 'Screen2',
+    isHidden: false,
+    conditionalLogic: null,
+    id: '2',
+    order: 1,
+  },
+  {
+    question: 'question',
+    responseType: 'singleSelect',
+    order: 1,
+    timer: null,
+    responseValues: {
+      options: [
+        {
+          id: '1',
+          text: 'test',
+          image: null,
+          score: null,
+          tooltip: '',
+          isHidden: false,
+          color: null,
+          alert: 'Some alert',
+          value: 0,
+        },
+      ],
+    },
+    config: {
+      removeBackButton: false,
+      skippableItem: false,
+      randomizeOptions: false,
+      timer: null,
+      addScores: false,
+      setAlerts: false,
+      addTooltip: false,
+      setPalette: false,
+      additionalResponseOption: {
+        textInputOption: false,
+        textInputRequired: false,
+      },
+      autoAdvance: true,
+    },
+    name: 'name',
+    isHidden: false,
+    conditionalLogic: null,
+    id: '3',
+  },
+];
+
+const activitiesDto: ActivityDto[] = [
+  {
+    id: '1',
+    name: 'one',
+    description: 'description one',
+    splashScreen: null,
+    image: null,
+    showAllAtOnce: false,
+    isSkippable: true,
+    isReviewable: false,
+    isHidden: false,
+    responseIsEditable: true,
+    order: 0,
+    items: activityItems,
+  },
+];
+
+export const answersDto: AnalyticsAnswerDto[] = [
+  {
+    answer: 'Some answer 1 ',
+    createdAt: '2020-04-04 20:00',
+    itemIds: ['1', '2', '3'],
+    activityId: '1',
+  },
+  {
+    answer: 'Some answer 2',
+    createdAt: '2020-04-04 20:00',
+    itemIds: ['1', '2', '3'],
+    activityId: '2',
+  },
+  {
+    answer: 'Some answer 3',
+    createdAt: '2020-04-04 20:00',
+    itemIds: ['3'],
+    activityId: '3',
+  },
+];
+
+const encryptionMockService = {
+  decrypt: () =>
+    JSON.stringify([
+      {
+        value: [1],
+      },
+      {
+        value: [2],
+      },
+      {
+        value: '1',
+      },
+    ]),
+};
+
+const encryptionMockServiceWithEmptyAnswers = {
+  decrypt: () => '{}',
+};
+
+export const analyticsMapperInput = {
+  appletId: '1',
+  activitiesDto: activitiesDto,
+  answersDto: answersDto,
+  encryptionService: encryptionMockService,
+};
+
+export const analyticsMapperInputWithEmptyAnswers = {
+  appletId: '1',
+  activitiesDto: activitiesDto,
+  answersDto: answersDto,
+  encryptionService: encryptionMockServiceWithEmptyAnswers,
+};
+
+export const analyticsMapperInputWithEmptyActivities = {
+  appletId: '1',
+  activitiesDto: [],
+  answersDto: answersDto,
+  encryptionService: encryptionMockServiceWithEmptyAnswers,
+};
---
+++ src/entities/applet/model/tests/mappers.test.ts
@@ -0,0 +1,120 @@
+import {
+  appletDetailsResponse,
+  firstAppletDto,
+  completedEntityDto,
+  secondAppletDto,
+  appletDetailsDto,
+  themeDto,
+  activityRecordsDto,
+  analyticsMapperInput,
+  analyticsMapperInputWithEmptyAnswers,
+  themeDtoWithEmptyImages,
+  analyticsMapperInputWithEmptyActivities,
+} from './mappers.input.mock';
+import {
+  appletDetails,
+  appletTheme,
+  mappedActivities,
+  mappedAnalytics,
+  mappedAnalyticsWithEmptyAnswers,
+  mappedApplets,
+  appletThemeWithEmptyImages,
+} from './mappers.output.mock.ts';
+import {
+  mapDtoToRespondentMeta,
+  mapCompletedEntityFromDto,
+  mapAppletDtoToAppletVersion,
+  mapApplets,
+  mapAppletDetailsFromDto,
+  mapThemeFromDto,
+  mapActivitiesFromDto,
+  mapAppletAnalytics,
+} from '../mappers.ts';
+
+describe('Applet entity mappers tests', () => {
+  it('Should return mapped result for respondent meta', async () => {
+    const result = mapDtoToRespondentMeta(appletDetailsResponse);
+
+    expect(result).toEqual('Some nick');
+  });
+
+  it('Should return empty mapped result for respondent meta', async () => {
+    const result = mapDtoToRespondentMeta({
+      ...appletDetailsResponse,
+      // @ts-expect-error
+      respondentMeta: null,
+    });
+
+    expect(result).toEqual('');
+  });
+
+  it('Should return mapped result for completed entity', async () => {
+    const result = mapCompletedEntityFromDto(completedEntityDto);
+
+    expect(result).toEqual({
+      endAt: 1586016000000,
+      entityId: '0',
+      eventId: '3',
+    });
+  });
+
+  it('Should return applet version from Applet Dto', async () => {
+    const result = mapAppletDtoToAppletVersion(firstAppletDto);
+
+    expect(result).toEqual({ appletId: '1', version: '1.1.11' });
+  });
+
+  it('Should return mapped result for array of applet dtos', async () => {
+    const result = mapApplets([firstAppletDto, secondAppletDto]);
+
+    expect(result).toEqual(mappedApplets);
+  });
+
+  it('Should return mapped result for applet details dto', async () => {
+    const result = mapAppletDetailsFromDto(appletDetailsDto);
+
+    expect(result).toEqual(appletDetails);
+  });
+
+  it('Should return mapped result for applet theme dto', async () => {
+    const result = mapThemeFromDto(themeDto);
+
+    expect(result).toEqual(appletTheme);
+  });
+
+  it('Should return mapped result for applet theme dto with empty images', async () => {
+    const result = mapThemeFromDto(themeDtoWithEmptyImages);
+
+    expect(result).toEqual(appletThemeWithEmptyImages);
+  });
+
+  it('Should return null for undefined theme', async () => {
+    const result = mapThemeFromDto(null);
+
+    expect(result).toEqual(null);
+  });
+
+  it('Should return mapped result for activity records dto', async () => {
+    const result = mapActivitiesFromDto(activityRecordsDto);
+
+    expect(result).toEqual(mappedActivities);
+  });
+
+  it('Should return mapped result for applet analytics', async () => {
+    const result = mapAppletAnalytics(analyticsMapperInput);
+
+    expect(result).toEqual(mappedAnalytics);
+  });
+
+  it('Should return mapped result for applet analytics with empty answers', async () => {
+    const result = mapAppletAnalytics(analyticsMapperInputWithEmptyAnswers);
+
+    expect(result).toEqual(mappedAnalyticsWithEmptyAnswers);
+  });
+
+  it('Should return mapped result for applet analytics with empty activities array', async () => {
+    const result = mapAppletAnalytics(analyticsMapperInputWithEmptyActivities);
+
+    expect(result).toEqual({ activitiesResponses: [], id: '1' });
+  });
+});
