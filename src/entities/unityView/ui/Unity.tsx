6345e6e67e71592e89492e8dae7a7d8110020c6d
Felipe Imperio <felipeimp@Felipes-MacBook-Pro.local>
2024-08-19 15:53:21 -0700

    chore/fixing PR validations

--- src/entities/unityView/ui/Unity.tsx
+++ src/entities/unityView/ui/Unity.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from 'react';
-import { View } from 'react-native';
 
 import UnityView from '@azesmway/react-native-unity';
 
@@ -38,15 +37,13 @@ const Unity = (config: Props) => {
   }, []);
 
   return (
-    <View style={{ flex: 1 }}>
-      <UnityView
-        ref={unityRef}
-        style={{ flex: 1 }}
-        onUnityMessage={(result: any) => {
-          console.log('onUnityMessage', result?.nativeEvent.message);
-        }}
-      />
-    </View>
+    <UnityView
+      ref={unityRef}
+      style={{ flex: 1 }}
+      onUnityMessage={(result: any) => {
+        console.log('onUnityMessage', result?.nativeEvent.message);
+      }}
+    />
   );
 };
 
