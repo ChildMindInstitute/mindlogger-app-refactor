4eb4a54e8992ba92b9be943ebf7324565039a03c
Felipe Imperio <felipeimp@Felipes-MacBook-Pro.local>
2024-12-06 16:08:20 -0800

    mapping payload and fixing between and outside of time range

--- src/entities/conditional-logic/model/conditions.ts
+++ src/entities/conditional-logic/model/conditions.ts
@@ -228,8 +228,8 @@ export const isBetweenTimesRange = (
 
   return (
     selectedTimeInMinutes !== null &&
-    selectedTimeInMinutes >= timeToMinutes(minTime) &&
-    selectedTimeInMinutes <= timeToMinutes(maxTime)
+    selectedTimeInMinutes > timeToMinutes(minTime) &&
+    selectedTimeInMinutes < timeToMinutes(maxTime)
   );
 };
 
