b26639bef8771f9705f1e958a555cb798b742c4d
André Vital <andre@andrevital.com>
2025-02-27 11:36:33 -0600

    feat: Add Lato to font assets

--- assets/fonts/index.ts
+++ assets/fonts/index.ts
@@ -1,5 +1,7 @@
 const robotoFont = require('./Roboto.ttf');
 const robotoMediumFont = require('./Roboto_medium.ttf');
 const atkinsonHyperlegibleFont = require('./Atkinson Hyperlegible Regular.ttf');
+const latoFont = require('./Lato-Regular.ttf')
 
-export { robotoFont, robotoMediumFont, atkinsonHyperlegibleFont };
+export { atkinsonHyperlegibleFont, latoFont, robotoFont, robotoMediumFont };
+
