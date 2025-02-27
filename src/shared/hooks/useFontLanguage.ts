81d754335fc3285040ba192769bf44fd4f67959e
André Vital <andre@andrevital.com>
2025-02-27 11:58:46 -0600

    feat: Add useFontLanguage hook

---
+++ src/shared/hooks/useFontLanguage.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+import { FontLanguages } from '@tamagui/core';
+import i18next from 'i18next';
+
+export const useFontLanguage = () => {
+  const [fontLanguage, setFontLanguage] = useState<FontLanguages | 'default'>(
+    'default',
+  );
+  const appLanguage = i18next.language;
+
+  useEffect(() => {
+    setFontLanguage(() => {
+      appLanguage !== 'el' ? 'default' : 'el';
+    });
+  }, [appLanguage]);
+
+  return fontLanguage;
+};
