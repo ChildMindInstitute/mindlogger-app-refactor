610158a9d10bc98ec2be3816bbd2f37b159aa0df
Farmer Paul <paul.hh@metalab.com>
2025-03-03 16:49:17 -0400

    fix: use Lato Black instead of Lato Bold (#948)

--- android/app/src/main/assets/custom/index.ts
+++ android/app/src/main/assets/custom/index.ts
@@ -3,12 +3,12 @@ const robotoMediumFont = require('./Roboto-Medium.ttf');
 const robotoBoldFont = require('./Roboto-Bold.ttf');
 const atkinsonHyperlegibleFont = require('./Atkinson Hyperlegible Regular.ttf');
 const latoFont = require('./Lato-Regular.ttf')
-const latoBoldFont = require('./Lato-Bold.ttf')
+const latoBlackFont = require('./Lato-Black.ttf')
 
 export {
   atkinsonHyperlegibleFont,
   latoFont,
-  latoBoldFont,
+  latoBlackFont,
   robotoFont,
   robotoMediumFont,
   robotoBoldFont
