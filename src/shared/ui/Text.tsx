9aa5167cc8aa74ce0292fa6fa543512d991aa418
Farmer Paul <paul.hh@metalab.com>
2025-03-04 18:06:48 -0400

    fix: Correct Lato assets, default `lineHeight` (M2-8638) (#951)
    The wrong Lato font assets were being used, this change updates them.
    Also, a default `lineHeight` needs to be explicit throughout the app
    as Android and iOS make different assumptions for a reasonable default
    value. Chose 125% as a reasonable default, which is overridable if
    provided.

--- src/shared/ui/Text.tsx
+++ src/shared/ui/Text.tsx
@@ -2,6 +2,23 @@ import { Text as TGUIText, TextProps } from '@tamagui/core';
 
 type Props = TextProps;
 
-export const Text = (props: Props) => (
-  <TGUIText color="$black" fontFamily="$body" fontWeight="400" {...props} />
-);
+export const Text = ({
+  fontSize = 16,
+  lineHeight: lineHeightProp,
+  ...props
+}: Props) => {
+  const lineHeight =
+    lineHeightProp ??
+    (typeof fontSize === 'number' ? fontSize * 1.25 : undefined);
+
+  return (
+    <TGUIText
+      color="$black"
+      fontFamily="$body"
+      fontWeight="400"
+      fontSize={fontSize}
+      lineHeight={lineHeight}
+      {...props}
+    />
+  );
+};
