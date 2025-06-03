e45024be641ce5e106d0deb5b4b56bb02e777ffc
Farmer Paul <paul.hh@metalab.com>
2025-06-03 13:37:04 -0300

    fix: Fix header backgrounds & top margin on iOS (#983) (M2-9329)
    The introduction of banners resulted in a layout & background colour issue on the assessment screens, an oversight on my part. There was both too big of a top gap, and the topmost header area behind the iPhone hardware no longer matched the background of the rest of the page. The background mismatch was happening on a handful of other screens during the assessment, including the intermediate answers submission screen during a flow, and the summary screen.
    
    This PR fixes it. It's only relevant to iOS devices, especially iPhone.

--- src/screens/ui/AutocompletionScreen.tsx
+++ src/screens/ui/AutocompletionScreen.tsx
@@ -1,12 +1,23 @@
 import { FC } from 'react';
 
+import { bannerActions } from '@app/entities/banner/model/slice';
+import { colors } from '@app/shared/lib/constants/colors';
+import { useAppDispatch } from '@app/shared/lib/hooks/redux';
+import { useOnFocus } from '@app/shared/lib/hooks/useOnFocus';
 import { Box } from '@app/shared/ui/base';
 import { ImageBackground } from '@app/shared/ui/ImageBackground';
 import { AutoCompletion } from '@app/widgets/survey/ui/completion/AutoCompletion';
 
 export const AutocompletionScreen: FC = () => {
+  const dispatch = useAppDispatch();
+
+  useOnFocus(() => {
+    // Match <ImageBackground> raster image top pixel color
+    dispatch(bannerActions.setBannersBg(colors.lightGrey4));
+  });
+
   return (
-    <Box bg="$secondary" flex={1}>
+    <Box bg="$lightGrey4" flex={1}>
       <ImageBackground>
         <Box flex={1} pt={12} pb={34}>
           <AutoCompletion />
