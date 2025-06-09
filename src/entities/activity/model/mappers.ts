896d2aa6210454843441f3ed9db1866ec15460bc
Elsie He <hello@elsiehe.com>
2025-06-09 14:42:01 -0700

    feat: add support for unity activity item type

--- src/entities/activity/model/mappers.ts
+++ src/entities/activity/model/mappers.ts
@@ -35,6 +35,7 @@ import {
   TimeItemDto,
   TimeRangeItemDto,
   VideoItemDto,
+  UnityItemDto,
 } from '@app/shared/api/services/ActivityItemDto';
 import { ActivityDto } from '@app/shared/api/services/IActivityService';
 import { getMsFromSeconds } from '@app/shared/lib/utils/dateTime';
@@ -191,6 +192,28 @@ function mapToDrawing(dto: DrawingItemDto): ActivityItem {
     isHidden: dto.isHidden,
     ...mapAdditionalText(dto.config),
     ...mapConditionalLogic(dto.conditionalLogic),
+  };
+}
+
+function mapToUnity(dto: UnityItemDto): ActivityItem {
+  return {
+    id: dto.id,
+    name: dto.name,
+    inputType: 'Unity',
+    config: {
+      file: dto.config.file,
+    },
+    timer: null,
+    order: dto.order,
+    question: dto.question,
+    isSkippable: false,
+    hasAlert: false,
+    hasScore: false,
+    isAbleToMoveBack: true,
+    hasTextResponse: false,
+    canBeReset: false,
+    hasTopNavigation: false,
+    isHidden: dto.isHidden,
   };
 }
 
@@ -874,6 +897,8 @@ export function mapToActivity(dto: ActivityDto): ActivityDetails {
           return mapToTime(item);
         case 'requestHealthRecordData':
           return mapToRequestHealthRecordData(item);
+        case 'unity':
+          return mapToUnity(item);
       }
     }),
     hasSummary: dto.scoresAndReports?.showScoreSummary ?? false,
