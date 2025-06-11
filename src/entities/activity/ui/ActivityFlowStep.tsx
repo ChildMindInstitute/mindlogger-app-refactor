2963958ef2f8adf895bee46b6021f5648c3adf28
sultanofcardio <14842108+sultanofcardio@users.noreply.github.com>
2025-06-11 17:27:53 -0500

    Remove unnecessary try-catch

--- src/entities/activity/ui/ActivityFlowStep.tsx
+++ src/entities/activity/ui/ActivityFlowStep.tsx
@@ -14,39 +14,31 @@ type Props = BoxProps & {
 };
 
 export const ActivityFlowStep: FC<Props> = props => {
-  try {
-    const { activity, hasOpacity } = props;
+  const { activity, hasOpacity } = props;
 
-    const {
-      activityPositionInFlow,
-      numberOfActivitiesInFlow,
-      activityFlowName,
-    } = activity.activityFlowDetails!;
+  const { activityPositionInFlow, numberOfActivitiesInFlow, activityFlowName } =
+    activity.activityFlowDetails!;
 
-    return (
-      <XStack {...props}>
-        <Image
-          source={badge}
-          width={18}
-          height={18}
-          style={{
-            marginRight: 3,
-            marginTop: IS_ANDROID ? 2 : 0,
-            opacity: 0.6,
-          }}
-        />
+  return (
+    <XStack {...props}>
+      <Image
+        source={badge}
+        width={18}
+        height={18}
+        style={{
+          marginRight: 3,
+          marginTop: IS_ANDROID ? 2 : 0,
+          opacity: 0.6,
+        }}
+      />
 
-        <Text
-          color="$darkGrey"
-          accessibilityLabel="activity-card-flow"
-          opacity={hasOpacity ? 0.5 : 1}
-        >
-          {`(${activityPositionInFlow} of ${numberOfActivitiesInFlow}) ${activityFlowName}`}
-        </Text>
-      </XStack>
-    );
-  } catch (error) {
-    console.error('Error rendering ActivityFlowStep:', error);
-    return <Text>Error rendering ActivityFlowStep</Text>;
-  }
+      <Text
+        color="$darkGrey"
+        accessibilityLabel="activity-card-flow"
+        opacity={hasOpacity ? 0.5 : 1}
+      >
+        {`(${activityPositionInFlow} of ${numberOfActivitiesInFlow}) ${activityFlowName}`}
+      </Text>
+    </XStack>
+  );
 };
