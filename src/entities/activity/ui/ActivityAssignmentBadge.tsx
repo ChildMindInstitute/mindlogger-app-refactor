7fbe629d8975c7a4dee51469716ab02c80b50042
farmerpaul <paul.hh@metalab.com>
2025-06-22 15:43:36 -0300

    chore: Delete old commented code

--- src/entities/activity/ui/ActivityAssignmentBadge.tsx
+++ src/entities/activity/ui/ActivityAssignmentBadge.tsx
@@ -22,34 +22,6 @@ export const ActivityAssignmentBadge: FC<Props> = ({
   const { shortName } = useActivityAssignee({ assignment });
 
   return (
-    // <XStack
-    //   alignItems="center"
-    //   py={6}
-    //   px={9}
-    //   borderRadius={8}
-    //   bg="$yellow_light"
-    // >
-    //   <Box
-    //     alignItems="center"
-    //     justifyContent="center"
-    //     width={16}
-    //     height={16}
-    //     mr={9}
-    //     bg="$yellow_light"
-    //     borderRadius={3}
-    //   >
-    //     <UserProfileIcon color={palette.yellow} size={12} />
-    //   </Box>
-    //   <Text
-    //     fontWeight="400"
-    //     fontSize={14}
-    //     lineHeight={20}
-    //     letterSpacing={0.1}
-    //     {...props}
-    //   >
-    //     {t('assignment:badge', { name: shortName })}
-    //   </Text>
-    // </XStack>
     <Chip
       variant="warning"
       icon={() => <UserProfileIcon color={palette.warning} size={12} />}
