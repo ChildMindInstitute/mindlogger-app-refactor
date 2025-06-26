9bf98440b4f986e7005b0f8d53fda3e99bfc822e
Elsie He <hello@elsiehe.com>
2025-06-26 09:52:23 -0700

    fix: directory check for prebuild AndroidManifest.xml patch

--- script/unity/prebuild.sh
+++ script/unity/prebuild.sh
@@ -8,8 +8,10 @@ echo "[Unity][Pre-build] Removing snapshotted UnityFramework.framework ..."
 rm -fr $SCRIPT_DIR/../../node_modules/@azesmway/react-native-unity/ios/UnityFramework.framework
 
 echo "[Unity][Pre-build] Removing <intent-filter /> from AndroidManifest.xml ..."
-awk '/<intent-filter>/ {skip=1} !skip {print} /<\/intent-filter>/ {skip=0}' \
-  $SCRIPT_DIR/../../unity/builds/android/unityLibrary/src/main/AndroidManifest.xml \
-  > $SCRIPT_DIR/../../unity/builds/android/unityLibrary/src/main/AndroidManifest.xml.modified
-mv $SCRIPT_DIR/../../unity/builds/android/unityLibrary/src/main/AndroidManifest.xml.modified \
-  $SCRIPT_DIR/../../unity/builds/android/unityLibrary/src/main/AndroidManifest.xml
+if [ -d $SCRIPT_DIR/../../unity/builds/android ]; then
+  awk '/<intent-filter>/ {skip=1} !skip {print} /<\/intent-filter>/ {skip=0}' \
+    $SCRIPT_DIR/../../unity/builds/android/unityLibrary/src/main/AndroidManifest.xml \
+    > $SCRIPT_DIR/../../unity/builds/android/unityLibrary/src/main/AndroidManifest.xml.modified
+  mv $SCRIPT_DIR/../../unity/builds/android/unityLibrary/src/main/AndroidManifest.xml.modified \
+    $SCRIPT_DIR/../../unity/builds/android/unityLibrary/src/main/AndroidManifest.xml
+fi
