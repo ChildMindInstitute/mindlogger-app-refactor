683c9c7ebadb1a3c6159767f83a5515afe25faa0
sricharan varanasi <59170910+sricharan-varanasi@users.noreply.github.com>
2026-05-19 10:56:36 -0400

    fix: update code for MMKV v4 and Skia 2.4.21 API changes

--- src/shared/lib/storages/createStorage.ts
+++ src/shared/lib/storages/createStorage.ts
@@ -1,35 +1,35 @@
-import { MMKV } from 'react-native-mmkv';
+import { createMMKV } from 'react-native-mmkv';
 
 import { AsyncStorage } from './AsyncStorage';
 import { registerMMKVStorage } from './ReactotronMMKVTracker';
 import { SyncStorage } from './SyncStorage';
 
 export function createSecureStorage(id: string, encryptionKey: string) {
-  const storage = new MMKV({ id, encryptionKey });
+  const storage = createMMKV({ id, encryptionKey });
   registerMMKVStorage(id, storage);
   return storage;
 }
 
 export function createStorage(id: string) {
-  const storage = new MMKV({ id });
+  const storage = createMMKV({ id });
   registerMMKVStorage(id, storage);
   return storage;
 }
 
 export function createSyncStorage(id: string) {
-  const mmkv = new MMKV({ id });
+  const mmkv = createMMKV({ id });
   registerMMKVStorage(id, mmkv);
   return new SyncStorage(mmkv);
 }
 
 export function createAsyncStorage(id: string) {
-  const mmkv = new MMKV({ id });
+  const mmkv = createMMKV({ id });
   registerMMKVStorage(id, mmkv);
   return new AsyncStorage(mmkv);
 }
 
 export function createSecureAsyncStorage(id: string, encryptionKey: string) {
-  const mmkv = new MMKV({ id, encryptionKey });
+  const mmkv = createMMKV({ id, encryptionKey });
   registerMMKVStorage(id, mmkv);
   return new AsyncStorage(mmkv);
 }
