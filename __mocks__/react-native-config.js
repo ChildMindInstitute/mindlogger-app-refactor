03bfc38489c11731bd458f5e6c71ac6c55709e3e
adeiji <adeiji@yahoo.com>
2026-06-25 14:52:37 -0700

    chore: add comments explaining test mock changes for RN 0.85 and nitro-sound migration

--- __mocks__/react-native-config.js
+++ __mocks__/react-native-config.js
@@ -1,3 +1,8 @@
+// Jest manual mock for react-native-config.
+// In RN 0.85, the bridge was removed and react-native-config now uses
+// TurboModuleRegistry which returns null in Jest. This mock provides
+// empty config values so tests can run without the native module.
+// See: https://github.com/luggit/react-native-config#jest
 export default {
   ENV: '',
   API_URL: '',
