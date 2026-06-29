5831466be9242c3d0ec77ce0e1cab5cf7b70ad3e
adeiji <adeiji@yahoo.com>
2026-06-29 08:14:24 -0700

    fix: patch react-native-unity for RN 0.85 Fabric architecture

--- script/unity/patch-react-native-unity.sh
+++ script/unity/patch-react-native-unity.sh
@@ -5,6 +5,8 @@
 # For example:
 # $ ./script/unity/patch-react-native-unity.sh ./node_modules/@azesmway/react-native-unity ./react-native-unity.patch
 
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
 echo "[Unity][Patch react-native-unity] Patching UPlayer.java ..."
 if [ -d $1/android.disabled ]; then
   cd $1/android.disabled/src/main/java/com/azesmwayreactnativeunity
@@ -12,3 +14,10 @@ else
   cd $1/android/src/main/java/com/azesmwayreactnativeunity
 fi
 patch --forward -p0 < $2 || [ $? -eq 1 ]
+
+# RN 0.85 Fabric: updateProps is not reliably called, so Unity never initializes.
+# This patch adds [self initUnityModule] to the new arch initWithFrame, matching
+# the old arch behavior.
+echo "[Unity][Patch react-native-unity] Patching RNUnityView.mm (Fabric init fix) ..."
+cd $1/ios
+patch --forward -p0 < $SCRIPT_DIR/react-native-unity-ios-fabric-init.patch || [ $? -eq 1 ]
