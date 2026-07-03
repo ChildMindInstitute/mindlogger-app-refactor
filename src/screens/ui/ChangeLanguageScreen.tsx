4a92dd740088c34f9d70189356111b7490f34dc1
adeiji <adeiji@yahoo.com>
2026-07-02 17:41:50 -0700

    fix: fixed ChangeLanguage crash on Android (M2-10958)

--- src/screens/ui/ChangeLanguageScreen.tsx
+++ src/screens/ui/ChangeLanguageScreen.tsx
@@ -8,7 +8,9 @@ export const ChangeLanguageScreen: FC = () => {
   const navigation = useNavigation();
 
   const onLanguageChanged = () => {
-    navigation.goBack();
+    requestAnimationFrame(() => {
+      navigation.goBack();
+    });
   };
 
   return (
