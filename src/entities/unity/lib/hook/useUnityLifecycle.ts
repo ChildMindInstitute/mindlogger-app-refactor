61edf4e64df707729055eb57cb0774f0bb7502dd
Chris Lei <chris.lei@childmind.org>
2026-09-24 13:49:16 -0700

    Maintain portrait orientation while Unity loads

--- src/entities/unity/lib/hook/useUnityLifecycle.ts
+++ src/entities/unity/lib/hook/useUnityLifecycle.ts
@@ -335,7 +335,9 @@ export const useUnityLifecycle = (options: UseUnityLifecycleOptions) => {
     registerEventHandler(UnityEventDataExport, handleDataExport);
   }, [handleDataExport, registerEventHandler]);
 
-  // Handle orientation change requests from Unity, re-lock to portrait on unmount.
+  // Handle orientation change requests from Unity
+  // - Keep portrait lock from app mount until Unity sends SetOrientation
+  // - Lock to portrait again when Unity unmounts
   const handleSetOrientation = useCallback<RNUnityCommBridgeUnityEventHandler>(
     msg => {
       if (msg.m_sKey === UnityEventSetOrientation) {
@@ -354,6 +356,7 @@ export const useUnityLifecycle = (options: UseUnityLifecycleOptions) => {
 
         const orientation = orientationMap[orientationValue];
         if (orientation !== undefined) {
+          // Lock to orientation specified by Unity in SetOrientation
           RNOrientationDirector.lockTo(orientation);
         } else {
           logger.warn(
@@ -366,8 +369,8 @@ export const useUnityLifecycle = (options: UseUnityLifecycleOptions) => {
   );
   useEffect(() => {
     registerEventHandler(UnityEventSetOrientation, handleSetOrientation);
-    RNOrientationDirector.unlock();
     return () => {
+      // Lock to portrait again when Unity unmounts
       RNOrientationDirector.lockTo(Orientation.portrait);
     };
   }, [handleSetOrientation, registerEventHandler]);
